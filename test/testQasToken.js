0e7ecba747486dd1a1d76a55e0a63c5ff9c93ae4
parkgoeeun <alfzltm101@naver.com>
2018-04-12 21:29:43 +0900

    write testcode for upvote

--- test/testQasToken.js
+++ test/testQasToken.js
@@ -48,4 +48,18 @@ contract('qasToken', function(accounts) {
       assert.equal(receipt.logs[0].args._value.toNumber(), bounty, "value must be " + bounty);
     });
   });
+
+  it("should upvote", function() {
+    return qasToken.deployed().then(function(instance) {
+      return instance.upVote(questionee, {
+        from: master
+      });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "one event should have been triggered");
+      assert.equal(receipt.logs[0].event, "Transfer", "event should be Transfer");
+      assert.equal(receipt.logs[0].args._from, master, "to account must be " + master);
+      assert.equal(receipt.logs[0].args._to, questionee, "from account must be " + questionee);
+      assert.equal(receipt.logs[0].args._value.toNumber(), bounty, "value must be " + bounty);
+    });
+  });
 });
