cfe6e6808ae7ea0b87c796440ef7f50a58bd4530
learnitdeep <learnitdeep@gmail.com>
2018-05-02 17:57:30 +0900

    Security Token1

--- security/token/KookminSecurityToken.sol
+++ security/token/KookminSecurityToken.sol
@@ -12,8 +12,15 @@ contract KookminSecurityToken is EIP20Interface {
     using SafeMath for uint256;
     
     uint256 constant private MAX_UINT256 = 2**256 - 1;
+    uint256 public githubCount;
+    uint256 public tokenCount;
+    uint256 public githubDone;
+    uint256 public tokenDone;
     mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public allowed;
+    mapping (uint256 => bytes32) public github;
+    mapping (uint256 => address) public token;
+    mapping (address => bytes32) public result;
     /*
     NOTE:
     The following variables are OPTIONAL vanities. One does not have to include them.
@@ -24,8 +31,8 @@ contract KookminSecurityToken is EIP20Interface {
     uint8 public decimals;                // How many decimals to show. I would choose 18.
     string public symbol;                 // KST
     
-    // 1 ether = 500 KST
-    uint256 public constant RATE = 5000;
+    // 1 ether = 50000 KST
+    uint256 public constant RATE = 50000;
     
     address public owner;
     
@@ -44,7 +51,9 @@ contract KookminSecurityToken is EIP20Interface {
         name = _tokenName;                                   // Set the name for display purposes
         decimals = _decimalUnits;                            // Amount of decimals for display purposes
         symbol = _tokenSymbol;                               // Set the symbol for display purposes
-        owner = msg.sender;                                  // Owner is me!
+        owner = msg.sender;   // Owner is me!
+        githubCount = 0;
+        tokenCount = 0;
     }
     
     function createTokens() payable{
@@ -91,12 +100,62 @@ contract KookminSecurityToken is EIP20Interface {
         return allowed[_owner][_spender];
     }
     
-    function sourceAnalysis(bytes32 _github, bytes32 _email, uint256 _value) public returns (bool success) {
-        require(balances[msg.sender] >= _value && _value > 0);
-        balances[msg.sender] = balances[msg.sender].sub(_value);
-        balances[owner] = balances[owner].add(_value);
-        emit Transfer(msg.sender, owner, _value); //solhint-disable-line indent, no-unused-vars
+    function githubAnalysis(bytes32 _github) public returns (bool success) {
+        githubCount = githubCount.add(1);
+        github[githubCount] = _github;
+        uint256 tokens = githubCount.add(tokenCount).add(1).sub(githubDone).sub(tokenDone);
+        balances[msg.sender] = balances[msg.sender].add(tokens);
+        totalSupply = totalSupply.add(tokens);
+        
         return true;
+    }
+    
+    function tokenAnalysis(address _token) public returns (bool success) {
+        uint256 tokens = githubCount.add(tokenCount).add(1).sub(githubDone).sub(tokenDone);
+        tokenCount = tokenCount.add(1);
+        token[tokenCount] = _token;
+        balances[msg.sender] = balances[msg.sender].add(tokens);
+        totalSupply = totalSupply.add(tokens);
+        
+        return true;
+    }
+    
+    function addTokenDone() public returns (bool success) {
+        require(msg.sender == owner);
+        tokenDone = tokenDone.add(1);
+        return true;
+    }
+    
+    function addGithubDone() public returns (bool success) {
+        require(msg.sender == owner);
+        githubDone = githubDone.add(1);
+        return true;
+    }
+    
+    function setResult(address _token, bytes32 _result) public returns (bool success) {
+        require(msg.sender == owner);
+        result[_token] = _result;
+        return true;
+    }
+    
+    function getResult(address _token) public view returns (bytes32 token_result) {
+        return result[_token];
+    }
+    
+    function getGithubCount() public view returns (uint256 github_Count) {
+        return githubCount;
+    }
+    
+    function getTokenCount() public view returns (uint256 token_Count) {
+        return tokenCount;
+    }
+    
+    function getGithub(uint256 num) public view returns (bytes32 github_address) {
+        return github[num];
+    }
+    
+    function getToken(uint256 num) public view returns (address token_address) {
+        return token[num];
     }
     
 }
