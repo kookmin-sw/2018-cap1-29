af25d9e764905fd268583eb386da8ead5fbfd8b3
learnitdeep <learnitdeep@gmail.com>
2018-05-25 11:00:52 +0900

    deploy question to ethereum network

--- web/ether.js
+++ web/ether.js
@@ -54,3 +54,32 @@ var txOptions = {
 // set template
 // var result = myContractInstance.getTokenCount();
 // console.log(result)
+
+var http = require('http');
+
+// 1. 요청한 url을 객체로 만들기 위해 url 모듈사용
+var url = require('url');
+// 2. 요청한 url 중에 Query String 을 객체로 만들기 위해 querystring 모듈 사용
+var querystring = require('querystring');
+
+var server = http.createServer(function(request,response){
+    // 3. 콘솔화면에 로그 시작 부분을 출력
+    console.log('--- log start ---');
+    // 4. 브라우저에서 요청한 주소를 parsing 하여 객체화 후 출력
+    var parsedUrl = url.parse(request.url);
+    console.log(parsedUrl);
+    // 5. 객체화된 url 중에 Query String 부분만 따로 객체화 후 출력
+    var parsedQuery = querystring.parse(parsedUrl.query,'&','=');
+    var rawTx = txutils.functionTx(interface, 'registQuestion', [Number(parsedQuery['coin']),parsedQuery['question'],parsedQuery['content']], txOptions);
+    sendRaw(rawTx);
+    console.log(parsedQuery['question']);
+    // 6. 콘솔화면에 로그 종료 부분을 출력
+    console.log('--- log end ---');
+
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end('Transaction has been sent.');
+});
+
+server.listen(3000, function(){
+    console.log('Server is running...');
+});
